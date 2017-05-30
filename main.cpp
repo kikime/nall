75ebdcfcb7d27aaff8d1d25f049644459232912d
Niclas <njdm.evil@gmail.com>
2017-05-30 21:07:40 +0200

    Added basic argv parsing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,66 @@
+#include <cstring>
+#include <iostream>
+
+namespace nall
+{
+  const unsigned int version = 0;
+
+  /**
+   * Checks a parameter against a long and a short form
+   *
+   * @param param the parameter to check
+   * @param l     the long form  (without --)
+   * @param s     the short form (without -)
+   */
+  bool paramcheck(const char *param, const char *l, char s)
+  {
+    if (*param != '-')
+      return false;
+
+    ++param;
+
+    if (*param == s)
+      return param[1] == '\0';
+
+    if (*param != '-')
+      return false;
+
+    ++param;
+
+    return !strcmp(param, l);
+  }
+}
+
 int main(int argc, char *argv[])
 {
+  if (argc == 1) {
+    std::cout << "nall v" << nall::version << " by Niclas Meyer (@_njdm).\n\nTry `" << argv[0] << " -h` for help.\n";
+    return 0;
+  }
+
+  bool sourceSpecified = false;
+  // Go over argv
+  for (int i = 1; i < argc; ++i) {
+    if (nall::paramcheck(argv[i], "help", 'h')) {
+      // TODO: Write help
+
+      return 0;
+    }
+
+    if (nall::paramcheck(argv[i], "version", 'v')) {
+      // TODO: Write version / about
+
+      return 0;
+    }
+
+    if (!sourceSpecified) {
+      sourceSpecified = true;
+      // TODO: Remember source
+    } else {
+      std::cout << "Multiple sources specified. Watch out.\n";
+      return -1;
+    }
+  }
 
   return 0;
 }
